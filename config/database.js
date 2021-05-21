01abdd04ccbbf48d1d1bc8c116b224f2db3bbf21
pierre edimo <pierredimo@live.com>
2021-05-21 22:31:40 +0200

    delete useless files

--- config/database.js
+++ config/database.js
@@ -3,8 +3,18 @@ module.exports = ({ env }) => ({
   connections: {
     default: {
       connector: 'mongoose',
-      settings: {},
-      options: {},
+      settings: {
+        host: env('DATABASE_HOST', 'cluster0.vnsp0.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'portfolio-backend'),
+        username: env('DATABASE_USERNAME', 'pierre'),
+        password: env('DATABASE_PASSWORD', 'janvier'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
     },
   },
 });
